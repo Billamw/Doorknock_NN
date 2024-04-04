5b3aefe999664581426611af2d3b5946653ba0ba
Billamw <66533754+Billamw@users.noreply.github.com>
2024-04-04 14:43:24 +0200

    made pt dataset and changed some files

--- doorknock.py
+++ doorknock.py
@@ -3,11 +3,13 @@ import torchaudio
 import pyaudio
 import numpy as np
 import librosa
+import time
 
-model = torch.load("doorknock_model.pt")
+model = torch.load("data/models/KnnmodelV2-2.pt")
 
 p = pyaudio.PyAudio()
 
+print("Öffne Mikrofon...")
 stream = p.open(
     format=pyaudio.paInt16,
     channels=1,
@@ -16,20 +18,30 @@ stream = p.open(
     frames_per_buffer=1024,
 )
 
-
+print("Starte Klopfen-Erkennung...")
 while True:
     # Audiodaten vom Mikrofon lesen
-    data = stream.read(1024)
+    start = time.time()
+    data = stream.read(3*1024)
+    end = time.time()
+    print(f"Time taken: {end-start}")
 
-    # Daten in ein Numpy-Array konvertieren
-    np_data = np.frombuffer(data, dtype=np.int16)
+    np_data = np.frombuffer(data, dtype=np.float32)
 
-    # MFCCs berechnen
-    mfccs = librosa.feature.mfcc(np_data, sr=22050)
+    # Überprüfen, ob die Daten endliche Werte enthalten
+    if not np.all(np.isfinite(np_data)):
+        print("Warning: Audio data contains non-finite values. Replacing with zeros.")
+        np_data = np.nan_to_num(np_data)
 
+    spectrum = librosa.feature.melspectrogram(y=np_data, sr=44100, n_mels=128, fmax=8000)
+    # flatten the spectrum
+    spectrum = spectrum.flatten()
+    print(spectrum.shape)
+    end = time.time()
+    print(f"Time taken: {end-start}")
     # Daten in ein Tensor konvertieren
-    tensor = torch.from_numpy(mfccs).float()
-
+    tensor = torch.from_numpy(spectrum).float().to("cuda")
+    print(tensor.shape)
     # Vorhersage des Modells
     prediction = model(tensor)
 
